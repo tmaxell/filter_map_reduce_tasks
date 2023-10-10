de058008e33ece926fe4686bac84c0780189a359
Tim Makhel <tmaxelhard@gmail.com>
2023-10-10 22:20:20 +0300

    Всё переписано на функции (без циклов)

--- fp_task_2_1.py
+++ fp_task_2_1.py
@@ -1,9 +1,5 @@
-from functools import reduce
-import statistics
-import numbers
-
 students = [
-    {"name": "Grisha", "age": 20, "subject": "Math", "grades": [76, 80, 64, 78]},
+    {"name": "Grigoriy", "age": 20, "subject": "Math", "grades": [76, 80, 64, 78]},
     {"name": "Viktor", "age": 18, "subject": "Math", "grades": [91, 85, 79, 88]},
     {"name": "Ivan", "age": 19, "subject": "Physics", "grades": [70, 71, 64, 84]},
     {"name": "Alexander", "age": 19, "subject": "Math", "grades": [86, 67, 74, 78]},
@@ -24,72 +20,31 @@ students = [
     {"name": "Sophia", "age": 19, "subject": "Math", "grades": [68, 87, 73, 79]},
     {"name": "Vladislav", "age": 18, "subject": "Math", "grades": [75, 88, 69, 71]},
 ]
-print("Введите возраст, по которому хотите отфилтьровать студентов:")
+print("Введите возраст, по которому хотите отфильтроват студентов:")
 target_age = int(input())
-print("Введите предмет, по которому хотите сделать выборку студентов:")
-target_subject = input()
+def calculate_average_grade(student):
+    grades = student["grades"]
+    return sum(grades) / len(grades)
 
-def calculate_average_grades(students):
-    student_averages = []
-    total_sum = 0
-    total_count = 0
+# Фильтрация студентов возрастом 19 лет
+filtered_students = list(filter(lambda student: student["age"] == target_age, students))
 
-    for student in students:
-        grades = student["grades"]
-        average = sum(grades) / len(grades)
-        student_averages.append({"name": student["name"], "average": average})
-        total_sum += sum(grades)
-        total_count += len(grades)
+# Вычисление среднего балла каждого студента
+average_grades = list(map(calculate_average_grade, students))
 
-    overall_average = total_sum / total_count
-    return student_averages, overall_average
+# Нахождение студента с самым высоким средним баллом
+best_student = max(students, key=calculate_average_grade)
 
-def find_top_students(students):
-    student_averages, _ = calculate_average_grades(students)
-    top_students = []
+# Вывод результатов
+print("Студенты возрастом 19 лет:")
+for student in filtered_students:
+    print(f"{student['name']}: {student['age']} лет")
 
-    max_average = max(student["average"] for student in student_averages)
+print("\nСредний балл каждого студента:")
+for i, student in enumerate(students):
+    print(f"{student['name']}: {average_grades[i]:.2f}")
 
-    for student in student_averages:
-        if student["average"] == max_average:
-            top_students.append(student["name"])
+print("\nЛучший студент:")
+print(f"{best_student['name']}: {calculate_average_grade(best_student):.2f}")
 
-    return top_students
-student_averages, overall_average = calculate_average_grades(students)
-top_students = find_top_students(students)
-
-print("Средний балл для каждого студента:")
-for student in student_averages:
-    print(f"{student['name']}: {student['average']}")
-
-print(f"Общий средний балл по всем студентам: {overall_average}")
-
-print("Студент(ы) с самым высоким средним баллом:")
-for student in top_students:
-    print(student)
-def filter_students_by_age(students, target_age):
-    filtered_students_age = []
-    for student in students:
-        if student["age"] == target_age:
-            filtered_students_age.append(student)
-    return filtered_students_age
-
-def filter_students_by_subject(students, target_subject):
-    filtered_students = []
-    for student in students:
-        if student["subject"] == target_subject:
-            filtered_students.append(student)
-    return filtered_students
-
-# Примеры использования функций для фильтрации студентов
-filtered_by_age_20 = filter_students_by_age(students, target_age)
-filtered_by_subject_math = filter_students_by_subject(students, target_subject)
-
-print(f"Студенты с возрастом {target_age}:")
-for student in filtered_by_age_20:
-    print(student["name"])
-
-print("Студенты по предмету:")
-for student in filtered_by_subject_math:
-    print(student["name"])
 
---
+++ fp_task_2_2.py
@@ -0,0 +1,40 @@
+users = [
+    {"name": "Grigoriy", "expenses": [240, 80, 100, 78]},
+    {"name": "Viktor", "expenses": [91, 100, 79, 88]},
+    {"name": "Ivan", "expenses": [70, 71, 600, 84]},
+    {"name": "Alexander", "expenses": [140, 350, 74, 78]},
+    {"name": "Mikhail", "expenses": [90, 500, 76, 81]},
+    {"name": "Mark", "expenses": [78, 91, 200, 81]},
+    {"name": "Vica", "expenses": [91, 82, 86, 170]},
+    {"name": "Nikita", "expenses": [80, 73, 83, 68]},
+    {"name": "Maxim", "expenses": [84, 78, 62, 67]},
+    {"name": "Ksenia", "expenses": [90, 81, 92, 76]},
+    {"name": "Maria", "expenses": [94, 75, 92, 76]},
+    {"name": "Kirill", "expenses": [65, 81, 67, 76]},
+    {"name": "Tatyana", "expenses": [75, 82, 67, 77]},
+    {"name": "Yaroslav", "expenses": [64, 68, 65, 72]},
+    {"name": "Vasiliy", "expenses": [80, 79, 87, 65]},
+    {"name": "Varvara", "expenses": [89, 79, 84, 91]},
+    {"name": "Amelia", "expenses": [90, 82, 96, 77]},
+    {"name": "Alexey", "expenses": [75, 75, 65, 65]},
+    {"name": "Sophia", "expenses": [68, 87, 73, 79]},
+    {"name": "Vladislav", "expenses": [75, 88, 69, 71]},
+]
+def filter_users(user):
+    # Здесь можно установить критерий фильтрации
+    return sum(user["expenses"]) > 400
+
+filtered_users = list(filter(filter_users, users))
+
+def calculate_total_expenses(user):
+    return {"name": user["name"], "total_expenses": sum(user["expenses"])}
+
+users_with_total_expenses = list(map(calculate_total_expenses, filtered_users))
+
+total_expenses_all_users = sum(user["total_expenses"] for user in users_with_total_expenses)
+
+print("Отфильтрованные пользователи:")
+for user in users_with_total_expenses:
+    print(f"{user['name']}: {user['total_expenses']}")
+
+print(f"Общая сумма расходов всех отфильтрованных пользователей: {total_expenses_all_users}")
